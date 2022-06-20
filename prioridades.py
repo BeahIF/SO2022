38898fb79679e105f0eaefa35e84bcd17dd8c631
BeahIF <beatrizdacosta1@gmail.com>
2022-06-19 21:46:59 -0300

    prioridades finalizado

--- prioridades.py
+++ prioridades.py
@@ -3,56 +3,35 @@ import heapq as hq
 
 def prioridades(linhas,tempo):
     newDict = {}
-    # print(len(linhas))
     for i in linhas:
         original =  i.split("|")
         newDict[original[0]] = int(original[3])
-        # newDict.append({int(original[3]):original[0]})
-        # , original[1], original[2], original[4], original[5], original[6])) 
-    # print(len(newDict))
-    # print(newDict)
+ 
     ordena = {}
     for i in sorted(newDict, key = newDict.get):
         ordena[i]= newDict[i]
     finalizados = []
     print("iniciando o processo")
-    # print(ordena)
+    
     while(len(linhas)>0):
-        print(len(linhas))
 
-    #     hq.heapify(ordena)  
-    #     # ordem como ficou: 0 -> prioridade 1-> nome
-    # # 2 -> tempoDeExecução 3 -> prioridade
-    # # 4 -> UID 5-> qtdeMemoria
-    # # salvar em ordem quanto demorou cada processo
-    # # retirar sem fazer o append
-    #     print(ordena[0][0],':',ordena[0][1])  
-
-        for i in ordena:
-            print(i)
+        for i in ordena.copy():
             linha= encontra(i, linhas)
-            print(linha)
             if(linha == None):
                 del ordena[i]
             else:
                 original =  linha.split("|")
-        #         print(i[0],':',i[1],':',i[3])  
-
                 diminuiTempo = int(original[2])-int(tempo)
                 copia = str(original[0])+"|"+str(original[1])+"|"+str(diminuiTempo)+"|"+str(original[3])+"|"+str(original[4])+"|"+str(original[5])+"|"+str(int(original[6])+1) 
-
                 if(int(diminuiTempo)>0):
                     linhas.append(copia)
                 if(diminuiTempo==0):
-                    print("O processo "+str(original[0])+" demorou "+str(original[6])+" para terminar.")
+                    finalizados.append("O processo "+str(original[0])+" demorou "+str(original[6])+" para terminar.")
                 else:
-                    finalizados.append("Na CPU:"+str(original[0])+" faltam "+str(diminuiTempo)+" para concluir o processo.")
-                    # print("Na CPU:"+str(original[0])+" faltam "+str(diminuiTempo)+" para concluir o processo.")
+                    print("Na CPU:"+str(original[0])+" faltam "+str(diminuiTempo)+" para concluir o processo.")
                 linhas.remove(linha)
-    #             # index= index+1
-    #         hq.heapify(ordena)  
-
-    # print(ordena)
+    for i in finalizados:
+        print(i)
 def encontra(processoName, linhas):
     for i in linhas:
         
